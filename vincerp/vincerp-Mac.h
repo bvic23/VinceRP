d90a014d644c14a1ae50a3311b52627ab6aae78d
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-10-30 08:42:00 +0100

    Add Mac framework and test target

---
+++ vincerp/vincerp-Mac.h
@@ -0,0 +1,19 @@
+//
+//  vincerp-Mac.h
+//  vincerp-Mac
+//
+//  Created by Agnes Vasarhelyi on 30/10/15.
+//  Copyright © 2015 Viktor Belenyesi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for vincerp-Mac.
+FOUNDATION_EXPORT double vincerp_MacVersionNumber;
+
+//! Project version string for vincerp-Mac.
+FOUNDATION_EXPORT const unsigned char vincerp_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <vincerp_Mac/PublicHeader.h>
+
+
