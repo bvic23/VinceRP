8b326d4db9c29dbfe76edae76346e911934987bb
bvic23 <bvic23@gmail.com>
2015-10-31 10:19:37 +0100

    Refactor travis & circle config

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -o pipefail && \
+xcodebuild clean build test \
+  -project vincerp.xcodeproj \
+  -scheme vincerp \
+  -sdk iphonesimulator \
+  ONLY_ACTIVE_ARCH=NO \
+  GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
+  GCC_GENERATE_TEST_COVERAGE_FILES=YES \
+  CODE_SIGN_IDENTITY="" \
+  CODE_SIGNING_REQUIRED=NO
