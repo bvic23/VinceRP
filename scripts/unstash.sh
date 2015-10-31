ff1da78ae92983b8b0a4f6b986e70ce5bcd169ac
bvic23 <bvic23@gmail.com>
2015-10-31 18:32:34 +0100

    Try with freshly stashed framework

--- scripts/unstash.sh
+++ scripts/unstash.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 CARTHAGE_BINARY_DIR="Carthage/Build"
-IOS_BINARY_DIR="${CARTHAGE_BINARY_DIR}/iOS"
-IOS_ZIP="iOS.zip"
+CARTHAGE_ZIP="Carthage.zip"
 
-rm -rf "${IOS_BINARY_DIR}"
-mkdir -p "${IOS_BINARY_DIR}"
-curl -L https://www.googledrive.com/host/0B--nGoDU083Ma3hTMFNPVUlvbzA > "${IOS_BINARY_DIR}/${IOS_ZIP}"
-cd "${IOS_BINARY_DIR}"
-unzip "${IOS_ZIP}"
-rm -f "${IOS_ZIP}"
+rm -rf "${CARTHAGE_BINARY_DIR}"
+mkdir -p "${CARTHAGE_BINARY_DIR}"
+curl -L https://www.googledrive.com/host/0B--nGoDU083MUGoyenBlajFIOUk > "${CARTHAGE_BINARY_DIR}/${CARTHAGE_ZIP}"
+cd "${CARTHAGE_BINARY_DIR}"
+unzip "${CARTHAGE_ZIP}"
+rm -f "${CARTHAGE_ZIP}"
