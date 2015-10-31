123bc4dc2f85489516526de1aeb95591d82c79b5
bvic23 <bvic23@gmail.com>
2015-10-31 17:42:51 +0100

    Set destination and generate coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ xcodebuild clean build test \
   -project vincerp.xcodeproj \
   -scheme vincerp \
   -sdk iphonesimulator \
+  -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.0'
   ONLY_ACTIVE_ARCH=NO \
   GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
   GCC_GENERATE_TEST_COVERAGE_FILES=YES \
