d3535d6d86de7ec6eebda505cc0a185daac3cee2
bvic23 <bvic23@gmail.com>
2015-10-31 19:04:27 +0100

    Add destination

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-xcodebuild clean build test \
+set -o pipefail && xcodebuild clean build test \
   -project vincerp.xcodeproj \
   -scheme vincerp \
   -sdk iphonesimulator \
+  -enableCodeCoverage YES
+  -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.0'
   ONLY_ACTIVE_ARCH=NO \
   GCC_INSTRUMENT_PROGRAM_FLOW_ARCS=YES \
   GCC_GENERATE_TEST_COVERAGE_FILES=YES \
