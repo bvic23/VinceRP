f20fa5ef3068d47b77735ad7df6e633ff7d17cc5
bvic23 <bvic23@gmail.com>
2015-12-21 11:25:37 +0100

    Add line and file information to custom asserts

--- VinceRP/Common/Util/CommonConstants.swift
+++ VinceRP/Common/Util/CommonConstants.swift
@@ -3,10 +3,10 @@
 // Copyright (c) 2015 Viktor Belenyesi. All rights reserved.
 //
 
-@noreturn internal func abstractMethod(fn: String = __FUNCTION__) {
-    fatalError("\(fn) must be overriden in subclass implementations")
+@noreturn internal func abstractMethod(function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    fatalError("\(function) in \(file)[\(line)]: must be overriden in subclass implementations")
 }
 
-@noreturn internal func unreachableCode(fn: String = __FUNCTION__) {
-    fatalError("This part of \(fn) must be unreachable, yous should not see this")
+@noreturn internal func unreachableCode(function: String = __FUNCTION__, file: String = __FILE__, line: Int = __LINE__) {
+    fatalError("\(function) in \(file)[\(line)]: must be unreachable, yous should not see this")
 }
