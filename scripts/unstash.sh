b1e70a8dba9197a7a779f1c4036318d8ea8541fb
bvic23 <bvic23@gmail.com>
2016-01-23 17:17:06 +0100

    Bump dependency

--- scripts/unstash.sh
+++ scripts/unstash.sh
@@ -5,7 +5,7 @@ CARTHAGE_ZIP="Carthage.zip"
 
 rm -rf "${CARTHAGE_BINARY_DIR}"
 mkdir -p "${CARTHAGE_BINARY_DIR}"
-curl -L https://www.googledrive.com/host/0B--nGoDU083MUGoyenBlajFIOUk > "${CARTHAGE_BINARY_DIR}/${CARTHAGE_ZIP}"
+curl -L https://www.googledrive.com/host/0B--nGoDU083MMUtaZHNxVHZhNUE > "${CARTHAGE_BINARY_DIR}/${CARTHAGE_ZIP}"
 cd "${CARTHAGE_BINARY_DIR}"
 unzip "${CARTHAGE_ZIP}"
 rm -f "${CARTHAGE_ZIP}"
