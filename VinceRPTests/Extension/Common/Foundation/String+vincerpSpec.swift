f42a43f1b2add225eb3c8305ed08a41996061627
bvic23 <bvic23@gmail.com>
2016-01-24 08:52:07 +0100

    Add trim tests

--- VinceRPTests/Extension/Common/Foundation/String+vincerpSpec.swift
+++ VinceRPTests/Extension/Common/Foundation/String+vincerpSpec.swift
@@ -15,33 +15,77 @@ class StringVinceRPSpec: QuickSpec {
         describe("length") {
             
             it("works with empty string") {
-                expect("".length) =~ 0
+                expect("".length) == 0
             }
             
             it("works with spaces only") {
-                expect("    ".length) =~ 4
+                expect("    ".length) == 4
             }
 
             it("works with english alphabet") {
-                expect("abcd".length) =~ 4
+                expect("abcd".length) == 4
             }
 
             it("works with spaces inside") {
-                expect("a  d".length) =~ 4
+                expect("a  d".length) == 4
             }
 
             it("works with hungarian alphabet") {
-                expect("ŐÚÁŰ".length) =~ 4
+                expect("ŐÚÁŰ".length) == 4
             }
 
             it("works with special characters") {
-                expect("…^~≠".length) =~ 4
+                expect("…^~≠".length) == 4
             }
 
             it("works with emojis") {
-                expect("😀🙏ű^".length) =~ 4
+                expect("😀🙏ű^".length) == 4
             }
 
+        }
+        
+        describe("trim") {
+            
+            it("works with empty string") {
+                // when
+                let result = "".trim()
+                
+                // then
+                expect(result) == ""
+            }
+
+            it("works with spaces only") {
+                // when
+                let result = "    ".trim()
+                
+                // then
+                expect(result) == ""
+            }
+            
+            it("works with spaces only") {
+                // when
+                let result = "abcd".trim()
+                
+                // then
+                expect(result) == "abcd"
+            }
+            
+            it("works with spaces only") {
+                // when
+                let result = "a  d".trim()
+                
+                // then
+                expect(result) == "a  d"
+            }
+            
+            it("works with spaces only") {
+                // when
+                let result = "    ŐÚÁŰ ".trim()
+                
+                // then
+                expect(result) == "ŐÚÁŰ"
+            }            
+            
         }
         
     }
